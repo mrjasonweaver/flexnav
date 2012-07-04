78a466af3d0e7ac12ce453407c71b572464e19e3
Jason Weaver <indyplanets@gmail.com>
2012-07-04 02:52:02 -0500

    reformated js

--- js/jquery.flexnav.js
+++ js/jquery.flexnav.js
@@ -1,4 +1,3 @@
-
 /*global jQuery */
 /*!	
 * FlexNav.js 0.2
@@ -10,58 +9,59 @@
 * Date: Thu July 4 01:12:00 2012 -0600
 */
 
-(function ( $ ) {
+ (function($) {
 
-	$.fn.flexNav = function( options ) {
-    var settings = $.extend( {
-      'breakpoint' : '800',
-      'animationSpeed' : 'fast'
-    }, options);
-		
-		var $this = $(this);
-		
-		var resizer = function() {
-			if ($(window).width() < settings.breakpoint) {
-				$("body").removeClass("lg-screen").addClass("sm-screen");	
-				$this.hide();	
-			} 
-			else {
-				$("body").removeClass("sm-screen").addClass("lg-screen");	
-			}
-			if ($(window).width() >= settings.breakpoint) {
-				$this.show();
-			}
-			
-		};
+    $.fn.flexNav = function(options) {
+        var settings = $.extend({
+            'breakpoint': '800',
+            'animationSpeed': 'fast'
+        },
+        options);
 
-    // Call once to set.
-    resizer();
+        var $this = $(this);
 
-		// Function for testing touch screens
-		function is_touch_device() {
-		  return !!('ontouchstart' in window);
-		} 
+        var resizer = function() {
+            if ($(window).width() < settings.breakpoint) {
+                $("body").removeClass("lg-screen").addClass("sm-screen");
+                $this.hide();
+            }
+            else {
+                $("body").removeClass("sm-screen").addClass("lg-screen");
+            }
+            if ($(window).width() >= settings.breakpoint) {
+                $this.show();
+            }
 
-		// Set class on html element for touch/no-touch
-		if (is_touch_device()) { 
-			$('html').addClass('flexNav-touch');
-		} else {
-			$('html').addClass('flexNav-no-touch');
-		}	
-			
-		// Toggle for nav menu
-		$('.menu-button').click(function() {
-			$this.slideToggle(settings.animationSpeed);			
-		});									
+        };
 
-		// Toggle click for sub-menus on touch and or small screens
-		$('.item-with-ul').click(function() {
-			$(this).find('.sub-menu').slideToggle(settings.animationSpeed);
-		});	
-				
-		// Call on resize. 
-		$(window).on('resize', resizer);
+        // Call once to set.
+        resizer();
 
-	};
-		
-})( jQuery );
+        // Function for testing touch screens
+        function is_touch_device() {
+            return !! ('ontouchstart' in window);
+        }
+
+        // Set class on html element for touch/no-touch
+        if (is_touch_device()) {
+            $('html').addClass('flexNav-touch');
+        } else {
+            $('html').addClass('flexNav-no-touch');
+        }
+
+        // Toggle for nav menu
+        $('.menu-button').click(function() {
+            $this.slideToggle(settings.animationSpeed);
+        });
+
+        // Toggle click for sub-menus on touch and or small screens
+        $('.item-with-ul').click(function() {
+            $(this).find('.sub-menu').slideToggle(settings.animationSpeed);
+        });
+
+        // Call on resize.
+        $(window).on('resize', resizer);
+
+    };
+
+})(jQuery);
